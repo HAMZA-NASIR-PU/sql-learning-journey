b7a5be686f3ee89a5fdadad13aec378f46547d79
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2025-10-23 14:19:36 +0500

    Add solution variants of Leetcode 1398

--- Leetcode/1398.sql
+++ Leetcode/1398.sql
@@ -39,14 +39,30 @@ SELECT product_name = 'C'
 FROM Orders;
 
 
+
+SELECT customer_id, product_name = 'A', product_name = 'B', product_name = 'C'
+FROM Orders;
+
+
+
+SELECT 
+	c.customer_id, 
+	SUM(o.product_name = 'A') AS bought_A,
+	SUM(o.product_name = 'B') AS bought_B,
+	SUM(o.product_name = 'C') AS bought_C
+FROM Customers c
+JOIN Orders o ON c.customer_id = o.customer_id
+GROUP BY 1;
+
+
 -- Solution 1.
 
 WITH cte AS
 (
   SELECT customer_id FROM Orders
   GROUP BY customer_id
-  HAVING SUM(product_name = 'A') > 0 AND SUM(product_name = 'B') > 0 AND SUM(product_name = 'C')
-);
+  HAVING SUM(product_name = 'A') > 0 AND SUM(product_name = 'B') > 0 AND SUM(product_name = 'C') = 0
+)
 
 SELECT * FROM Customers
 WHERE customer_id IN (SELECT customer_id FROM cte);
@@ -70,3 +86,35 @@ HAVING
 ORDER BY 
     c.customer_id;
 
+
+
+
+SELECT c.customer_id, c.customer_name
+FROM Customers c
+JOIN Orders o ON c.customer_id = o.customer_id
+GROUP BY 1, 2
+HAVING SUM(o.product_name = 'A') > 0 AND SUM(o.product_name = 'B') AND SUM(o.product_name = 'C') = 0;
+
+
+-- Solution 3
+
+SELECT customer_id, customer_name
+FROM
+    Customers
+    LEFT JOIN Orders USING (customer_id)
+GROUP BY 1
+HAVING SUM(product_name = 'A') > 0 AND SUM(product_name = 'B') > 0 AND SUM(product_name = 'C') = 0
+ORDER BY 1;
+
+-- Solution 4
+
+SELECT c.*, r.*
+FROM customers c JOIN (
+SELECT customer_id, 
+	SUM(product_name = 'A') AS bought_A,
+    SUM(product_name = 'B') AS bought_B,
+    SUM(product_name = 'C') AS bought_C
+FROM orders
+GROUP BY customer_id
+) r ON c.customer_id = r.customer_id
+WHERE r.bought_A > 0 AND r.bought_B > 0 AND r.bought_C = 0;
